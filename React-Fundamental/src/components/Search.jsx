c95e1e51cdc2ae258da3f80ac590afbd2509e0e0
Adeka <adkhawildanrizqia@gmail.com>
2025-11-09 03:46:57 +0700

    Child to Parent

---
+++ React-Fundamental/src/components/Search.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function Search(props) {
+  const [search, setSearch] = useState("");
+
+  const onSearchChange = (event) => {
+    setSearch(event.target.value);
+    props.onSearchChange(event.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        Cari Artikel : <input onChange={onSearchChange} type="text" />
+      </div>
+      <small>Ditemukan 0 data dengan pencarian {search}</small>
+    </>
+  );
+}
+
+export default Search;
--- React-Fundamental/src/pages/Index.jsx
+++ React-Fundamental/src/pages/Index.jsx
@@ -1,26 +1,30 @@
 import Article from "../components/article";
 import posts from "../data/Posts.json";
+import Search from "../components/Search";
 
 import { useState } from "react";
 
 function HomePage() {
-  const [search, setSearch] = useState("");
-  const changeSearch = (event) => {
-    setSearch(event.target.value);
+  const [filteredPosts, setFilteredPosts] = useState(posts);
+
+  const onSearchChange = (value) => {
+    const filtered = posts.filter((item) => item.title.includes(value));
+    setFilteredPosts(filtered);
   };
 
   return (
     <>
       <h1>Simple Blog</h1>
-      <div>
-        Cari Artikel : <input onChange={changeSearch} type="text" />
-      </div>
-      <small>Ditemukan 0 data dengan pencarian {search}</small>
+      <Search onSearchChange={onSearchChange} />
+
       {/* {posts.map((blog) => (
         <Article key={blog.title} title={blog.title} tags={blog.tags} date={blog.date} />
       ))} */}
-      {posts.map(({ title, tags, date }, index) => (
+      {/* {posts.map(({ title, tags, date }, index) => (
         <Article {...{ title, tags, date }} key={index} />
+      ))} */}
+      {filteredPosts.map((props, index) => (
+        <Article {...props} key={index} />
       ))}
     </>
   );
