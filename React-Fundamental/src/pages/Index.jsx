7fd508d2d7108dc941343dc90ba8fb748662e6b5
Adeka <adkhawildanrizqia@gmail.com>
2025-11-10 01:35:36 +0700

    UseEffect and API

--- React-Fundamental/src/pages/Index.jsx
+++ React-Fundamental/src/pages/Index.jsx
@@ -2,17 +2,24 @@ import Article from "../components/article";
 import posts from "../data/Posts.json";
 import Search from "../components/Search";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function HomePage() {
   const [filteredPosts, setFilteredPosts] = useState(posts);
   const [totalPost, setTotalPost] = useState(0);
+  const [externalPosts, setExternalPosts] = useState([]);
 
   const onSearchChange = (value) => {
     const filtered = posts.filter((item) => item.title.includes(value));
     setFilteredPosts(filtered);
     setTotalPost(filtered.length);
   };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setExternalPosts(json));
+  }, []);
 
   return (
     <>
@@ -27,6 +34,11 @@ function HomePage() {
       ))} */}
       {filteredPosts.map((props, index) => (
         <Article {...props} key={index} />
+      ))}
+      <hr />
+      <h2>External Posts</h2>
+      {externalPosts.map((item, index) => (
+        <div key={index}> - {item.title}</div>
       ))}
     </>
   );
